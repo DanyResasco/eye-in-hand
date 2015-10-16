5ce5da7790e1737e6b0db622b0afde7b6f039446
DanyResasco <daniela.resasco@gmail.com>
2015-10-16 18:40:56 +0200

    update

--- src/prova_camera_dany.cpp
+++ src/prova_camera_dany.cpp
@@ -2,6 +2,7 @@
 #include <camera_hand.h>
 
 using namespace cv;
+RNG rng(12345);
 
 int main(int argc, char** argv)
 {
@@ -69,7 +70,7 @@ void Camera::ControllCamera()
 	 	{
 	 		//std::cout<<"ciao dany non ho la cam accesa"<<std::endl;
        		//	std::cout<<"Unable to read stream from specified device"<<std::endl;
-       		scene = imread("/home/daniela/Desktop/pollini/bott.jpg", CV_LOAD_IMAGE_UNCHANGED);
+       		scene = imread("/home/daniela/Desktop/pollini/bott1.jpg", CV_LOAD_IMAGE_UNCHANGED);
 		    if(!scene.data ) // Check for invalid input
 		    {
 		        std::cout<<"Could not open or find the image"<<std::endl; 
@@ -83,8 +84,10 @@ void Camera::ControllCamera()
 	 	{
 	 		//set the callback function for any mouse event
 			setMouseCallback("CAMERA_ROBOT", CallBackFunc, NULL);
-			if(press_buttom ==1 )	//wait the mouse event
+			//std::cout<<"preso bott"<<std::endl;
+			if(press_buttom == 1 )	//wait the mouse event
 			{
+				// std::cout<<"prima di shape"<<std::endl;
 				ShapeDetect();
 			}
 		}
@@ -97,6 +100,9 @@ void Camera::ControllCamera()
 
 void Camera::ShapeDetect()
 { 
+	// std::vector<geometry> Shape_;
+
+
 	Mat src_gray;
     /// Convert it to gray
   	cvtColor( scene, src_gray, CV_BGR2GRAY );
@@ -106,23 +112,22 @@ void Camera::ShapeDetect()
   	// Convert to binary image using Canny
 	Mat bw;
 	cv::Canny(src_gray, bw, 0, 50, 5);
-
+	
 	// Find contours
 	std::vector<std::vector<cv::Point> > contours;
 	cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
+	// Shape_.resize(contours.size());
 	// The array for storing the approximation curve
 	std::vector<cv::Point> approx;
 
 	// We'll put the labels in this destination image
 	cv::Mat dst = scene.clone();
-	// // std::vector<Point2f>center(contours.size()  );
-	// // std::vector<Moments> mu(contours.size() );
-	// std::vector<std::vector<cv::Point> > circle;
-	// // std::vector<float>radius_cerchio( contours.size() );
-  	int num_bott_cerchio = 0;
-  	// std::vector<std::vector<cv::Point> > circle;
+	// std::vecto<int> = Index_Shape;
+	std::vector<cv::Point>  Center_Shape;
+	std::vector<std::vector<cv::Point> > Shape_local;;
 
+	//std::cout<<"trovati i contorni"<<std::endl;
 	for (int i = 0; i < contours.size(); i++)
 	{
 	    // Approximate contour with accuracy proportional
@@ -157,14 +162,18 @@ void Camera::ShapeDetect()
 	        // to determine the shape of the contour
 	        if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3)
 	        {
-	            // Detect rectangle or square
+	            // Detect geometry.Shape_contours or square
 	            cv::Rect r = cv::boundingRect(contours[i]);
 	            double ratio = std::abs(1 - (double)r.width / r.height);
 
 	            setLabel(dst, ratio <= 0.02 ? "SQU" : "RECT", contours[i]);
+	            
+	            Center_Shape.push_back(FindACenter(contours[i]));
+	            Shape_local.push_back(contours[i]);
+	            
 	        }
 	        else if (vtc == 5 && mincos >= -0.34 && maxcos <= -0.27)
-	            setLabel(dst, "PENTA", contours[i]);
+	             setLabel(dst, "PENTA", contours[i]);
 	        else if (vtc == 6 && mincos >= -0.55 && maxcos <= -0.45)
 	            setLabel(dst, "HEXA", contours[i]);
     	}
@@ -178,115 +187,108 @@ void Camera::ShapeDetect()
 	        if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 && std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
 	        {
 
-	        	//GetCenter(contours[i]);
 	            setLabel(dst, "CIR", contours[i]);
-	            num_bott_cerchio ++;
-	            circle.push_back(contours[i]);
-	            //std::cout<<"numero di cerchi "<<num_bott_cerchio<<std::endl;
-	            //std::cout<<"circle.size(): "<<circle.size()<<std::endl;
+	            Center_Shape.push_back(FindACenter(contours[i]));
+	            Shape_local.push_back(contours[i]);
+	                  
 	        }
 	    }
 	}
-	//FindACenter()
-	std::vector<Point2f>center(circle.size()  );
-	std::vector<Moments> mu(circle.size() );
-	std::vector<float>radius_cerchio( circle.size() );
-	std::vector<int> distance;
-	std::vector<Point2f> mc( circle.size() );
 
-	if(num_bott_cerchio > 0)
+	// std::cout<<"center size: "<<Center_Shape.size()<<std::endl;
+	// std::cout<<"contours size: "<<Shape_local.size()<<std::endl;
+
+	std::pair<int, bool> info_geometry;
+
+	info_geometry = FindAMinDistanceButton(Center_Shape);
+
+	if(info_geometry.second == true)
 	{
-		for(int i=0; i<circle.size();i++)
-		{	// Get the moments
-		   	mu[i] = moments( circle[i], false );
-		   	// std::cout<<"mu: "<<mu[i].m10 <<mu[i].m00 <<mu[i].m01 <<std::endl;
-		   	mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
-		   	// std::cout<<"mc: "<<mc[i]<<std::endl;
-		   	int  local_dist;
-	 		cv::Point baricentro;
-	 		baricentro.x = floor(mc[i].x);
-			baricentro.y = floor(mc[i].y);
-			// std::cout<<"center i x: "<<baricentro.x <<std::endl;
-			// std::cout<<"center i y: "<<baricentro.y <<std::endl;
-
-			local_dist = norm((pos_object - baricentro));
-			distance.push_back(local_dist);
-		}
-				
-		//std::cout<<"distance.size: "<<distance.size()<<std::endl; 
-
-		int min_d = distance[0];
-		int index_circle;
-		for(int i=0; i< distance.size(); i++)
-		{
-		   	if(min_d > distance[i])
-		   	{
-				min_d = distance[i];
-		   		index_circle = i;
-		   		//std::cout<<"index circle: "<<index_circle<<std::endl;
-		   	}
-					  	
-		  	else
-		 	{
-				continue;
-		  	}
-
-		}
-		// std::cout<<"min_d: "<<min_d<<std::endl;
-		if(min_d > 90)
-		{
-			// first_Step = 1;
-			std::cout<<"Non è stato premuto correttamente il pulsante"<<std::endl;
-			std::cout<<"Premere il pulsante desiderato"<<std::endl;
-		}
-		else
-		{
-			first_Step = 0;
-			press_buttom = 0;
-			CorretObjectPos.x = floor(mc[index_circle].x);
-			CorretObjectPos.y = floor(mc[index_circle].y);
-			//std::cout<<"hai premuto: "<<CorretObjectPos.x<<  CorretObjectPos.y <<std::endl;
-			setLabel(dst, "BOTP", circle[index_circle]);
-			BottonCHosen = circle[index_circle];
-			cv::imshow("dst", dst);
-			cv::waitKey(0);
-		}
+	
+		first_Step = 0;
+		press_buttom = 0;
+		CorretObjectPos.x = floor(Center_Shape[info_geometry.first].x);
+		CorretObjectPos.y = floor(Center_Shape[info_geometry.first].y);
+		//std::cout<<"hai premuto: "<<CorretObjectPos.x<<  CorretObjectPos.y <<std::endl;
+		setLabel(dst, "BOTP", Shape_local[info_geometry.first]);
+		BottonCHosen = Shape_local[info_geometry.first];
+		cv::imshow("dst", dst);
+		cv::waitKey(0);
 	}
 	else
 	{
-		std::cout<<"nessun bottone trovato"<<std::endl;
+		std::cout<<"riprova"<<std::endl;
+	}
+		
+}
+
+
+std::pair<int, bool> Camera::FindAMinDistanceButton(std::vector<cv::Point> &baricentro)
+{
+	int  local_dist;
+	std::vector<int> distance;
+	std::pair<int, bool> check_bot;
+	check_bot.second = false;
+	
+	for(int i=0; i<= baricentro.size();i++)
+	{
+		local_dist = norm((pos_object - baricentro[i]));
+		// std::cout<<"distance 14: "<<local_dist<<std::endl;
+
+		distance.push_back(local_dist);	
 	}
 	
-	//cv::imshow("src", scene);
+
+	int min_d = distance[0];
+	check_bot.first = 0;
+
+	for(int i=0; i < distance.size(); i++)
+	{
+	   	if((min_d >= distance[i]) && (distance[i] < 90))
+	   	{
+			min_d = distance[i];
+	   		check_bot.first = i;
+	   		check_bot.second = true;
+	   		//std::cout<<"index: "<<index_shape <<std::endl;
+
+
+	   	}
+					  	
+	  	else
+	 	{
+	 		// std::cout<<"Non è stato premuto correttamente il pulsante"<<std::endl;
+	 		// std::cout<<"Premere nuovamente il pulsante"<<std::endl;
+			continue;
+	  	}
+	}
+
+
 	
-	
+
+	return check_bot;
 
 }
 
 
-void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+
+
+
+
+
+
+
+
+
+cv::Point FindACenter(std::vector<cv::Point> &geometry)
 {
-	int fontface = cv::FONT_HERSHEY_SIMPLEX;
-	double scale = 0.4;
-	int thickness = 1;
-	int baseline = 0;
+    cv::Moments m = moments(geometry, true);
+    cv::Point center(m.m10/m.m00, m.m01/m.m00);
 
-	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
-	cv::Rect r = cv::boundingRect(contour);
-
-	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
-	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+   return center;
 }
 
-static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
-{
-	double dx1 = pt1.x - pt0.x;
-	double dy1 = pt1.y - pt0.y;
-	double dx2 = pt2.x - pt0.x;
-	double dy2 = pt2.y - pt0.y;
-	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
-}
+
+
 
 void Camera::DetectAndMove(cv::Mat &frame)
 {
@@ -406,4 +408,27 @@ void Camera::CallBackFunc(int event, int x, int y, int flags, void* userdata)
 
 }
 
+void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+{
+	int fontface = cv::FONT_HERSHEY_SIMPLEX;
+	double scale = 0.4;
+	int thickness = 1;
+	int baseline = 0;
+
+	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
+	cv::Rect r = cv::boundingRect(contour);
+
+	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
+	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+}
+
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+{
+	double dx1 = pt1.x - pt0.x;
+	double dy1 = pt1.y - pt0.y;
+	double dx2 = pt2.x - pt0.x;
+	double dy2 = pt2.y - pt0.y;
+	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
 
